933a0d0ca715b03a08b8bd0d5ed2c4d7cfc694a8
Shubham Mengane <shubhammengane28@gmail.com>
2024-07-26 12:32:36 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HeaderComponent } from './header/header.component';
+import {HttpClientModule} from'@angular/common/http';
+import { DetailsComponent } from './details/details.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotfoundComponent,
+    HeaderComponent,
+    DetailsComponent,
+    AddemployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  empData: any;
+  date=new Date();
+  isSelect:boolean=false;
+  recordId!:number;
+
+  constructor(private service:HttpService,
+              private router:Router
+  ){
+
+  }
+
+  ngOnInit(): void {
+    this.GetAllRecordFromBackend();
+  }
+
+  GetAllRecordFromBackend(){
+    this.service.getAllRecord()
+    .subscribe((respose:any)=>{
+      //console.log(respose);
+      this.empData=respose;
+    })
+  }
+
+  onEdit(id:any){
+    console.log(id);
+    this.isSelect=true;
+    this.recordId=id;
+  }
+  onUpdate(){
+    if(this.isSelect){
+      // navigate to add employee
+          this.router.navigate(['/addemp',this.recordId]);
+    }else{
+      alert("Please Select a Record to be Update...")
+    }
+  }
+
+  onDelete(){
+    if(this.isSelect){
+      //Delete
+
+      if(confirm("Do you want Delete this record")){
+        this.service.deleteData(this.recordId)
+        .subscribe((response)=>{
+          this.GetAllRecordFromBackend();
+          alert(response)
+        });
+      }else{
+        alert("Record is Not Deleted...")
+      }
+
+      
+    }else{
+      alert("Please Select a record to delete....")
+    }
+  }
+ 
+
+
+}
